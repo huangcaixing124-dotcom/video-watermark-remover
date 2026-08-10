374800f52c9e2b779f5fddb0b9caa7282d522983
huangcaixing124-dotcom <huangcaixing124@gmail.com>
2026-08-10 09:08:19 +0800

    chore: 清理临时快捷方式脚本

--- server/rename_sh.ps1
+++
@@ -1,9 +0,0 @@
-$d = [Environment]::GetFolderPath('Desktop')
-$old = "$d\wm-tools.lnk"
-$new = "$d\去水印视频工具-控制台.lnk"
-if (Test-Path $old) {
-    Rename-Item $old $new
-    Write-Host "Renamed"
-} else {
-    Write-Host "Not found"
-}
